e98ec8505f16f2126cfdc8dcc0c125e7c9cdbd27
STNeto1 <gfreitasneto18@gmail.com>
2023-05-12 23:50:26 -0300

    dynamo setup

--- stacks/Api.ts
+++ stacks/Api.ts
@@ -1,7 +1,15 @@
-import { StackContext, Api } from "sst/constructs";
+import { StackContext, Api, use } from "sst/constructs";
+import { Database } from "./Database";
 
 export function API({ stack }: StackContext) {
+  const db = use(Database);
+
   const api = new Api(stack, "api", {
+    defaults: {
+      function: {
+        bind: [db]
+      }
+    },
     routes: {
       "GET /": "packages/functions/src/lambda.handler"
     }
